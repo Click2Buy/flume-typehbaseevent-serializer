b62d808f2ae25090eb54cbf2e21edce001273961
Fabien Carrion <fabien.carrion@gmail.com>
2019-08-02 15:12:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Flume Type HBase Serializer
+
+## Build
+mvn package
+
+## Install
+Copy target/flume-typehbaseevent-serializer-1.0.0.jar in Flume' classpath
+
+## Configure
+hbaseagent.sinks.prices.serializer = com.marketconnect.flume.serializer.TypeHbaseEventSerializer
+hbaseagent.sinks.prices.serializer.colNames = price,unified_price,available,created_at
+hbaseagent.sinks.prices.serializer.types = double,double,string,long
+
+It will force the type to double / string or long
---
+++ src/main/java/com/marketconnect/flume/serializer/TypeHbaseEventSerializer.java
@@ -0,0 +1,115 @@
+package com.marketconnect.flume.serializer;
+
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.FlumeException;
+import org.apache.flume.conf.ComponentConfiguration;
+import org.apache.flume.sink.hbase.HbaseEventSerializer;
+import org.apache.hadoop.hbase.client.Increment;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Row;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Lists;
+
+public class TypeHbaseEventSerializer implements HbaseEventSerializer {
+  // Config vars
+  /** Type used to send data to hbase. */
+  public static final String TYPES_CONFIG = "types";
+  public static final String TYPES_DEFAULT = "string";
+
+  /** Comma separated list of column names to place matching groups in. */
+  public static final String COL_NAME_CONFIG = "colNames";
+  public static final String COLUMN_NAME_DEFAULT = "col";
+
+  /** What charset to use when serializing into HBase's byte arrays */
+  public static final String CHARSET_CONFIG = "charset";
+  public static final String CHARSET_DEFAULT = "UTF-8";
+
+  protected byte[] cf;
+  private byte[] payload;
+  private Map<String, String> colNames;
+  private Map<String, String> headers;
+  private Charset charset;
+
+  @Override
+  public void configure(Context context) {
+    charset = Charset.forName(context.getString(CHARSET_CONFIG,
+        CHARSET_DEFAULT));
+
+    String typesStr = context.getString(TYPES_CONFIG, TYPES_DEFAULT);
+    String[] types = typesStr.split(",");
+
+    String colNameStr = context.getString(COL_NAME_CONFIG, COLUMN_NAME_DEFAULT);
+    String[] columnNames = colNameStr.split(",");
+
+    this.colNames = Maps.newHashMap();
+    for (int i = 0; i < columnNames.length; i++) {
+        String c = columnNames[i];
+        String t = types[i];
+        this.colNames.put(c, t);
+    }
+  }
+
+  @Override
+  public void configure(ComponentConfiguration conf) {
+  }
+
+  @Override
+  public void initialize(Event event, byte[] columnFamily) {
+    this.headers = event.getHeaders();
+    this.payload = event.getBody();
+    this.cf = columnFamily;
+  }
+
+  @Override
+  public List<Row> getActions() throws FlumeException {
+    List<Row> actions = Lists.newArrayList();
+    byte[] rowKey = this.payload;
+
+    if (rowKey.length == 0) {
+      return Lists.newArrayList();
+    }
+
+    try {
+      Put put = new Put(rowKey);
+
+      for (Map.Entry<String, String> entry : headers.entrySet()) {
+          String entryStr = entry.getKey();
+          String valueStr = entry.getValue();
+          String type = colNames.get(entryStr);
+          if ("string".equalsIgnoreCase(type)) {
+              put.add(cf, entryStr.getBytes(charset), Bytes.toBytes(valueStr));
+          } else if ("double".equalsIgnoreCase(type)) {
+              put.add(cf, entryStr.getBytes(charset), Bytes.toBytes(Double.parseDouble(valueStr)));
+          } else if ("float".equalsIgnoreCase(type)) {
+              put.add(cf, entryStr.getBytes(charset), Bytes.toBytes(Float.parseFloat(valueStr)));
+          } else if ("int".equalsIgnoreCase(type)) {
+              put.add(cf, entryStr.getBytes(charset), Bytes.toBytes(Integer.parseInt(valueStr)));
+          } else if ("long".equalsIgnoreCase(type)) {
+              put.add(cf, entryStr.getBytes(charset), Bytes.toBytes(Long.parseLong(valueStr)));
+          } else if ("short".equalsIgnoreCase(type)) {
+              put.add(cf, entryStr.getBytes(charset), Bytes.toBytes(Short.parseShort(valueStr)));
+          }
+      }
+      actions.add(put);
+    } catch (Exception e) {
+      throw new FlumeException(e);
+    }
+    return actions;
+  }
+
+  @Override
+  public List<Increment> getIncrements() {
+    return Lists.newArrayList();
+  }
+
+  @Override
+  public void close() {  }
+}
